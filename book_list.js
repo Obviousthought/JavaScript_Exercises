3e7d9f64ce1dc57d6c918c3105ef1a0b5acf7a10
Student <user@chromebox-001.(none)>
2013-10-17 16:40:43 -0700

    Book List semi-completed

--- book_list.js
+++ book_list.js
@@ -1,35 +1,86 @@
-var BookList = function (booksRead, notRead, nextBook, currentBook, lastBook) {
-    this.booksRead = booksRead;
-    this.notRead = notRead;
-    this.nextBook = nextBook;
-    this.currentBook = currentBook;
-    this.lastBook = lastBook;
+var BookList = function (bookArray) {
+    this.bookArray = bookArray;
+    this.booksRead = 0;
+    this.notRead = 0;
+    this.nextBook = null;
+    this.currentBook =null;
+    this.lastBook = null;
     this.add = function (Book) {
+        this.bookArray.push(Book);
         if (Book.bookReaded === true) {
             this.booksRead++;
+            this.lastBook = checkDates(this.bookArray);
         }
         else {
             this.notRead++;
         }
     };
+    this.finishBook = function(Book) {
+        bookIsThere = false;
+        for (var i = 0; i < bookArray.length; i++) {
+            if (Book === bookArray[i]) {
+                bookIsThere = true;
+            }
+        }
+        if (bookIsThere === true) {
+            Book.bookReaded = true;
+            Book.bookReadDate = new Date();
+            this.booksRead++;
+            this.notRead--;
+        }
+        else {
+            console.log("Sorry, that Book is not in this Book List.");
+        }
+
+    };
+
 };
+
+var checkDates = function(bookArray) {
+    var lastRead = bookArray[0];
+    for (var i =1; i < bookArray.length; i++) {
+        if (bookArray[i].bookReadDate >= lastRead.bookReadDate) {
+            lastRead = bookArray[i];
+        }
+    }
+    return lastRead;
+
+};
+
 
 var Book = function(bookTitle, bookGenre, bookAuthor, bookReaded, bookReadDate) {
     this.bookTitle = bookTitle;
     this.bookGenre = bookGenre;
     this.bookAuthor = bookAuthor;
     this.bookReaded = bookReaded;
-    if (bookReaded === true) {
-        this.bookReadDate = new Date(bookReadDate);
-    }
-    else {
-        this.bookReadDate = null;
-    }
+    this.bookReadDate = bookReadDate;
 };
 
-var spiderman = new Book("Spiderman", "Hero", "Geeky-dude", true, "May 19, 1990");
-console.log(spiderman);
+var spiderman = new Book("Spiderman", "Hero", "Geeky-dude", true, new Date(2000, 5, 4));
 var superman = new Book("Superman Adventures", "Hero", "Other geek", false);
-console.log(superman);
-var princessComic = new Book("Princess Stories", "Fantasy", "Little girl", true, "Sep 13, 1988");
-console.log(princessComic);
+var princessComic = new Book("Princess Stories", "Fantasy", "Little girl", true, new Date(2001,12,14));
+var sorcerersStone = new Book("sorcerersStone", "Fantasy", "Rowling", true, new Date(2013,05,10));
+var chamberOfSecrets = new Book("chamberOfSecrets", "Fantasy", "Rowling", true, new Date(2004,08,16));
+var prisonerOfAzcaban = new Book("prisonerOfAzcaban", "Fantasy", "Rowling", true, new Date(1995,04,04));
+var gobletOfFire = new Book("gobletOfFire","Fantasy", "Rowling", false);
+var orderOfThePhoenix = new Book("orderOfThePhoenix","Fantasy", "Rowling", false);
+var halfBloodPrince = new Book("halfBloodPrince","Fantasy", "Rowling", false);
+var deathlyHallows = new Book("deathlyHallows","Fantasy", "Rowling", false);
+
+var HarryPotter = new BookList([]);
+var ComicList = new BookList([]);
+
+ComicList.add(spiderman);
+ComicList.add(superman);
+ComicList.add(princessComic);
+
+HarryPotter.add(sorcerersStone);
+HarryPotter.add(chamberOfSecrets);
+HarryPotter.add(prisonerOfAzcaban);
+HarryPotter.add(gobletOfFire);
+HarryPotter.add(orderOfThePhoenix);
+HarryPotter.add(halfBloodPrince);
+HarryPotter.add(deathlyHallows);
+
+
+HarryPotter.finishBook(gobletOfFire);
